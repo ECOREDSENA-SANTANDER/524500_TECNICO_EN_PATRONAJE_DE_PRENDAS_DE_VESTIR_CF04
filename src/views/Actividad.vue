46f55eb37953289745522196253d59ed1157c8a0
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2024-08-13 09:50:34 -0500

    Cargue de actividad didáctica

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,17 +7,17 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/falso-verdadero.svg", alt="Texto que describa la imagen")
+            img(src='@/assets/curso/temas/actividad-didactica.svg', alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Nombre de la actividad
-        p.mb-4 #[b Objetivo] 
+          h2 Conceptos de trazado de patrones y la herramienta Optitex.
+        p.mb-4 #[b Identificar la apropiación de los contenidos en la temática sobre Trazado de patrones y la interfaz de Optitex] 
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Tipo de actividad
+              p.fw-bold.mb-0 Completar espacios
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
